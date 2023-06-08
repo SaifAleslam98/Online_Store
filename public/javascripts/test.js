d51dda247feb4b23067e6e19235c85ea72684792
Saif Aleslam <saifhassn94@gmail.com>
2023-06-08 17:23:27 +0200

    Updating header(nav-bar)

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -15,3 +15,33 @@ $(".tab-slider--nav li").click(function() {
   $(".tab-slider--nav li").removeClass("active");
   $(this).addClass("active");
 });
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+  document.getElementById("mySidenav").style.display = "flex";
+  document.getElementById("mySidenav").style.flexDirection = "column";
+
+  document.getElementById("mySidenav").style.alignItems = "center";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+
+}
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+      document.getElementById("nav-bar").classList.remove("nav-bar");
+      document.getElementById("nav-bar").classList.add("scroll-nav");
+      document.getElementById("nav-bar").style.height = "50px";
+
+
+  } else {
+      document.getElementById("nav-bar").classList.remove("scroll-nav");
+      document.getElementById("nav-bar").classList.add("nav-bar");
+      document.getElementById("nav-bar").style.height = "70px";
+
+  }
+}
